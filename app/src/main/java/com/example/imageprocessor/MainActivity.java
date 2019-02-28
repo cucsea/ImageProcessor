e295ffc3435c3b7da14fa061b8c559dc711a2889
Heron Zhang <zhlcuc@163.com>
2019-02-28 17:18:01 +0800

    Project Created
    Brain image  processor

---
+++ app/src/main/java/com/example/imageprocessor/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.imageprocessor;
+
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.uniquext.android.imageeditor.helper.DrawableManager;
+public class MainActivity extends AppCompatActivity {
+
+    private AppCompatImageView imageView;
+    Bitmap editBm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+        imageView.setImageResource(R.drawable.picture);
+        editBm = BitmapFactory.decodeResource(getResources(), R.drawable.picture);
+
+    }
+
+    public void chooseImg(View v){
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 10);
+        }else{
+            openAlbum();
+        }
+
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode,String[] permissons,int[] grantResult) {
+        switch (requestCode) {
+            case 10:
+                if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(MainActivity.this, "permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default: ;
+        }
+    }
+
+    public void openAlbum(){
+        Intent intent=new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,2);
+    }
+
+
+    public void editImg(View v){
+        DrawableManager.getInstance().init(editBm);
+        startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse("scheme://com.uniquext.image-editor/main")), 1);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case 1:
+                if (resultCode == DrawableManager.SAVE) {
+                    Bitmap bitmap = DrawableManager.getInstance().getDrawableBitmap();
+                    imageView.setImageBitmap(bitmap);
+                } else {
+                    Toast.makeText(this, "没有保存", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                if(resultCode==RESULT_OK){
+                    if(Build.VERSION.SDK_INT>=19){
+                        handleImageOnKitKat(data);
+                    }else{
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+
+            default:
+
+        }
+
+    }
+
+    private void handleImageOnKitKat(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            String docId=DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id=docId.split(":")[1];
+                String selection=MediaStore.Images.Media._ID+"="+id;
+                imagePath=getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri= ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath=getImagePath(contentUri,null);
+            }
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath=getImagePath(uri,null);
+        }else if("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath=uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+
+    private void handleImageBeforeKitKat(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+        imagePath=getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String Path=null;
+        Cursor cursor=getContentResolver().query(uri,null,selection,null,null);
+        if(cursor!=null){
+            if(cursor.moveToFirst()){
+                Path=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return Path;
+    }
+
+
+    private void displayImage(String Path){
+        editBm=BitmapFactory.decodeFile(Path);
+        imageView.setImageBitmap(editBm);
+    }
+}
